0d824ab0d2b351ac035c16fbd9e9193ce5859a8e
Karim Bend'chiche <bendchichek@gmail.com>
2023-12-26 23:13:40 +0100

    ptn

--- app/components/AuthForm.jsx
+++ app/components/AuthForm.jsx
@@ -11,7 +11,7 @@ export default function AuthForm() {
         view = "magic_link"
         showLinks = {false}
         providers = {[]}
-        redirectTo = 'https://liste-voeux.vercel.app/auth/callback'
+        redirectTo = 'https://liste-voeux-3kbybt6qf-karims-projects-050470a5.vercel.app/auth/callback'
         appearance = {{
             theme: 'dark',
             button: {
